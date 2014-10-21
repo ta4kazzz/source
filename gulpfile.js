9dac19957a9024459b3865a0d95a09dd16a6a2a0
Pat Riley <pat@bitpay.com>
2014-10-21 08:26:03 -0400

    adjusted sizing in reader and prepared for live reload

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var connect = require('gulp-connect');
 
 var paths = {
   sass: ['./scss/**/*.scss']
