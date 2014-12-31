94e56aabc9bf79d137dd5e728a1bd8ec1a89fcbb
Pat Riley <pat@bitpay.com>
2014-12-31 09:25:33 -0500

    changed app name and updated ionic

--- www/js/app.js
+++ www/js/app.js
@@ -4,11 +4,11 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('source', ['ionic', 'source.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // Hide the accessory bar by default (remove this to show the 2 bar above the keyboard
     // for form inputs)
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('source.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
   // Form data for the login modal
