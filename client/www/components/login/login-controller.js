e0b2e716e880032351a0f3aca15b2d8b7ae5fa91
Pat Riley <pat@bitpay.com>
2015-01-18 10:56:09 -0500

    login working

--- client/www/components/login/login-controller.js
+++ client/www/components/login/login-controller.js
@@ -3,51 +3,24 @@ angular.module('source')
 .controller('loginController', function($scope, $rootScope, API, $window) {
 
 
-     $scope.showLoginForm = true;
-
- 	// if the user is already logged in, take him to his bucketlist
-    if ($rootScope.isSessionActive()) {
-        $window.location.href = ('#/app/home');
-    }
-
+    $scope.showLoginForm = true;
 
 	$scope.user = {
-		email: "",
+		username: "",
 		password: ""
 	};
 
-	// We know this works
 
     $scope.validateUser = function () {
-        var email = this.user.email;
+        var username = this.user.username;
         var password = this.user.password;
 
-        if(!email || !password) {
-        	$rootScope.notify("Please enter valid credentials");
-        	return false;
-        }
 
+        $rootScope.setUsername(username);
+        $rootScope.setPassword(password);
 
-        // $rootScope.show('Please wait.. Authenticating');
-
-        API.login({
-            email: email,
-            password: password
-        }).success(function (data) {
-            $rootScope.setToken(email); // create a session kind of thing on the client side
-            $rootScope.hide();
-            $window.location.href = ('#/app/home');
-        }).error(function (error) {
-            $rootScope.hide();
-            $rootScope.notify("Invalid Username or password");
-        });
+        $window.location.href = ('#/app/home');
     }
-
-
-
-
-
-
 
 
 });
--- client/www/js/services.js
+++ client/www/js/services.js
@@ -23,52 +23,20 @@ angular.module('source.services', [])
     };
 
 
-
-
-
-    $rootScope.show = function(text) {
-    	$rootScope.loading = $ionicLoading.show({
-    		content: text ? text : 'Loading',
-    		animation: 'fade-in',
-    		showBackdrop: true,
-    		maxWidth: 200,
-    		showDelay: 0
-    	});
-    };
-
-
-    $rootScope.hide = function () {
-        $ionicLoading.hide();
-    };
-
     $rootScope.logout = function () {
-        $rootScope.setToken("");
+        // $rootScope.setToken("");
+        $rootScope.setPassword("");
+        $rootScope.setUsername("");
         $window.location.href = '#/app/login';
     };
 
-    $rootScope.notify =function(text){
-        $rootScope.show(text);
-        $window.setTimeout(function () {
-          $rootScope.hide();
-        }, 1999);
-    };
+    // $rootScope.setToken = function (token) {
+    //     return $window.localStorage.token = token;
+    // }
 
-    $rootScope.doRefresh = function (tab) {
-        if(tab == 1)
-            $rootScope.$broadcast('fetchAll');
-        else
-            $rootScope.$broadcast('fetchCompleted');
-        
-        $rootScope.$broadcast('scroll.refreshComplete');
-    };
-
-    $rootScope.setToken = function (token) {
-        return $window.localStorage.token = token;
-    }
-
-    $rootScope.getToken = function () {
-        return $window.localStorage.token;
-    }
+    // $rootScope.getToken = function () {
+    //     return $window.localStorage.token;
+    // }
 
     $rootScope.isSessionActive = function () {
         return $window.localStorage.token ? true : false;
