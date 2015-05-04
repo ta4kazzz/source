c4921978027317d7507d5f61dc8fb15c7d6c2bf9
Pat Riley <pat@bitpay.com>
2015-05-03 22:21:41 -0400

    switched to production

--- server/config/database.js
+++ server/config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    // 'url' : 'mongodb://heroku_app35106009:foor9p8ufms37jgda4j5ossqv5@ds053937.mongolab.com:53937/heroku_app35106009'
-    'url' : 'mongodb://localhost/source'
+    'url' : 'mongodb://heroku_app35106009:foor9p8ufms37jgda4j5ossqv5@ds053937.mongolab.com:53937/heroku_app35106009'
+    // 'url' : 'mongodb://localhost/source'
 };
