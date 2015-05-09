4f500690a2f23c51d6291315e8eca26f75cb13f8
Pat Riley <pat@bitpay.com>
2015-05-09 11:55:48 -0400

    added things to reader view

--- sourceTabs/www/js/controllers.js
+++ sourceTabs/www/js/controllers.js
@@ -759,16 +759,81 @@ $scope.getFollows = function() {
 
   API.getArticle(id)
     .success(function (article, status, headers, config) {
+      $scope.article = article;
+      $scope.articleUrl = article.url;
+      $scope.articleID = article._id;
       $scope.articleTitle = article.title;
       $scope.articleTime = article.created;
       $scope.articleImageUrl = article.imageUrl;
       $scope.articleContent = article.content;
+      $scope.articleShortUrl = article.shortUrl;
+      $scope.articleDate = article.created;
+      $scope.username = article.username;
+      $scope.usernameImage = article.gravatarURL;
 
 
     })
     .error(function (article, status, headers, config) {
       console.log("Something went wrong")
     });
+
+
+  };
+
+
+  $scope.saveForLater = function(articleID) {
+
+    var userID      = window.localStorage.SourceID;
+    var articleID    = $scope.articleID;
+
+    var savedArticle = {
+      articleID: articleID,
+      userID: userID
+    };
+
+    console.log(savedArticle);
+
+    API.saveForLater(savedArticle)
+      .success(function (article, user, status, headers, config) {
+        // make button reflect the change
+        console.log("Article successfully saved for later")
+      })
+      .error(function (article, status, headers, config) {
+        console.log("Error when saving the article for later")
+      });
+
+  };
+
+
+  $scope.likeArticle = function(article) {
+
+    // console.log("Like article triggered" + article);
+
+    var userID               = window.localStorage.SourceID;
+    var articleID            = article._id;
+    var articleOwner         = article._userID;
+    var articleImageUrl      = article.imageUrl;
+
+    // console.log("Article Owner Username is " + articleOwnerUsername);
+
+    var likedArticle = {
+      articleID: articleID,
+      imageUrl: articleImageUrl,
+      userID: userID,
+      created:  Date.now(),
+      articleOwner: articleOwner
+    };
+
+    console.log(likedArticle);
+
+    API.likeArticle(likedArticle)
+      .success(function (article, user, status, headers, config) {
+        // make button reflect the change
+        console.log("Article Successfully liked")
+      })
+      .error(function (article, status, headers, config) {
+        console.log("Error when liking the article")
+      });
 
 
   };
